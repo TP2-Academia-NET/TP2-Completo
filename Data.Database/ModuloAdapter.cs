7173c1bca1b7a7522dfc4f95365fad376e57e016
Marcelo Castellano <marce.geek22@gmail.com>
2016-11-08 21:16:12 -0300

    Agrego ModuloAdapter

---
+++ Data.Database/ModuloAdapter.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Data.Database
+{
+    public class ModuloAdapter : Adapter
+    {
+        public Business.Entities.Modulo GetOne(int ID)
+        {
+            Modulo mod = new Modulo();
+
+            try
+            {
+                sqlConn = this.OpenConnection();
+                SqlCommand cmdModulos = new SqlCommand("select * from modulos where id_modulo = @id", sqlConn);
+                cmdModulos.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drModulos = cmdModulos.ExecuteReader();
+
+                if (drModulos.Read())
+                {
+                    mod.ID = (int)drModulos["id_modulo"];
+                    mod.Descripcion = (string)drModulos["desc_modulo"];
+                }
+                drModulos.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos de Módulo", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return mod;
+        }
+
+        public List<Modulo> GetAll()
+        {
+            List<Modulo> modulos = new List<Modulo>();
+
+            try
+            {
+                sqlConn = this.OpenConnection();
+                SqlCommand cmdModulos = new SqlCommand("Select * from modulos", sqlConn);
+                SqlDataReader drModulos = cmdModulos.ExecuteReader();
+
+                while (drModulos.Read())
+                {
+                    Modulo mod = new Modulo();
+
+                    mod.ID = (int)drModulos["id_modulo"];
+                    mod.Descripcion = (string)drModulos["desc_modulo"];
+
+                    modulos.Add(mod);
+                }
+                drModulos.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos de Módulos", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return modulos;
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                sqlConn = this.OpenConnection();
+
+                SqlCommand cmdDelete = new SqlCommand("delete modulos where id_modulo = @id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar Módulo", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Update(Modulo modulo)
+        {
+            try
+            {
+                sqlConn = this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("UPDATE modulos SET desc_modulo = @desc_modulo WHERE id_modulo = @id", sqlConn);
+
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = modulo.ID;
+                cmdSave.Parameters.Add("@desc_modulo", SqlDbType.VarChar, 50).Value = modulo.Descripcion;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al modificar datos del Módulo", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Insert(Modulo modulo)
+        {
+            try
+            {
+                sqlConn = this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("insert into modulos (desc_modulo) values(@desc_modulo) select @@identity", sqlConn);
+
+                cmdSave.Parameters.Add("@desc_modulo", SqlDbType.VarChar, 50).Value = modulo.Descripcion;
+                modulo.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear módulo", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(Modulo modulo)
+        {
+            if (modulo.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(modulo.ID);
+            }
+            else if (modulo.State == BusinessEntity.States.New)
+            {
+                this.Insert(modulo);
+            }
+            else if (modulo.State == BusinessEntity.States.Modified)
+            {
+                this.Update(modulo);
+            }
+            modulo.State = BusinessEntity.States.Unmodified;
+        }
+    }
+}
